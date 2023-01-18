8b15055175f3d2f8bbbe43dd69327b12067e052a
sfsajid91 <sfsajid91@gmail.com>
2023-01-18 20:59:05 +0600

    added custom headers

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -2,6 +2,12 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const middlewares = [express.json(), morgan('dev'), cors()];
+// Custom headers
+const headers = (req, res, next) => {
+    res.header('X-Powered-By', 'NodeJS'); // X-Powered-By: NodeJS
+    next();
+};
+
+const middlewares = [express.json(), morgan('dev'), cors(), headers];
 
 module.exports = middlewares;
